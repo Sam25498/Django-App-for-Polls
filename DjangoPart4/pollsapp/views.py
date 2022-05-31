5b1564cd67c7165154c98b800dc13e362066ecd9
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-05-31 10:54:32 +0300

    Update views.py

--- DjangoPart4/pollsapp/views.py
+++ DjangoPart4/pollsapp/views.py
@@ -1,3 +1,6 @@
+###################################################################################################################################
+#                              OLD VIEWS.PY FILE CHECK BELOW FOR THE NEW ONE.                                                     #
+###################################################################################################################################
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -35,6 +38,41 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a 
         # user hits Back button.
         return HttpResponseRedirect(reverse('pollsapp:results', args=(question.id,)))
+    
+    
+###################################################################################################################################
+#                                                """New Ammended Views."""                                                        #
+###################################################################################################################################
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    ... # same as above, no changes needed.
+
    
 
 
