b697e44f35a082417b1ac4b6910025bc8e007a45
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-05-31 10:04:59 +0300

    Update views.py

--- DjangoPart4/pollsapp/views.py
+++ DjangoPart4/pollsapp/views.py
@@ -1,9 +1,23 @@
-from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Choice, Question
-# ...
+
+# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'pollsapp/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'pollsapp/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'pollsapp/results.html', {'question': question})
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -17,7 +31,10 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
+        #Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a 
+        # user hits Back button.
         return HttpResponseRedirect(reverse('pollsapp:results', args=(question.id,)))
+   
+
+
