e71e74e9bd02930caf6b519c0d52c9332570afc3
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-06-03 11:20:18 +0300

    Update admin.py

--- DjangoPart7/pollsapp/admin.py
+++ DjangoPart7/pollsapp/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
   fieldsets = [
         (None,               {'fields': ['question_text']}),
@@ -9,7 +13,6 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
   
-  
-  
+   
 admin.site.register(Question, QuestionAdmin) 
 
